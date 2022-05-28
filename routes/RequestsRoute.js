179cf5a4eb24e4ead6ef00ec07c4e6f86e3ef9e3
ZaidAli3642 <zaidali36422@gmail.com>
2022-05-29 01:13:38 +0500

    implemented followers, following routes and small changes

--- routes/RequestsRoute.js
+++ routes/RequestsRoute.js
@@ -25,7 +25,9 @@ router.post("/request", async (req, res) => {
       .returning("*");
 
     res.status(200).json({ message: "Request", request });
-  } catch (error) {}
+  } catch (error) {
+    res.status(400).json({ message: "Error occured.", error });
+  }
 });
 
 router.get("/request/:user_id", async (req, res) => {
@@ -61,6 +63,167 @@ router.patch("/approve/:join_id", async (req, res) => {
       .returning("*");
 
     res.status(200).json({ message: "Approved Request.", approvedRequest });
+  } catch (error) {
+    res.status(400).json({ message: "Error occured.", error });
+  }
+});
+
+router.post("/follow_request", async (req, res) => {
+  const { follow_user_id, follow_firstname, user_id } = req.body;
+
+  try {
+    const request = await db
+      .insert({
+        follow_user_id,
+        follow_firstname,
+        approve_request: false,
+        user_id,
+      })
+      .into("follow_request")
+      .returning("*");
+
+    res.status(200).json({ message: "Request", request });
+  } catch (error) {
+    res.status(400).json({ message: "Error occured.", error });
+  }
+});
+
+router.get("/follow_request/:user_id", async (req, res) => {
+  const { user_id } = req.params;
+
+  try {
+    const allRequests = await db
+      .select("*")
+      .from("follow_request")
+      .where(function () {
+        this.where("follow_request.user_id", "=", user_id).andWhere(
+          "follow_request.approve_request",
+          "=",
+          false
+        );
+      });
+    res.status(200).json({ message: "All Requests", allRequests });
+  } catch (error) {
+    res.status(400).json({ message: "Error occured.", error });
+  }
+});
+
+router.patch("/approve_follow/:follow_id", async (req, res) => {
+  const { follow_id } = req.params;
+
+  try {
+    const approvedRequest = await db
+      .update({
+        approve_request: true,
+      })
+      .from("follow_request")
+      .where({ follow_id })
+      .returning("*");
+
+    res.status(200).json({ message: "Request Approved.", approvedRequest });
+  } catch (error) {
+    res.status(400).json({ message: "Error occured.", error });
+  }
+});
+
+router.delete("/reject_request/:follow_id", async (req, res) => {
+  const { follow_id } = req.params;
+
+  try {
+    await db.del().from("follow_request").where({ follow_id });
+    res.status(200).json({ message: "Request Rejected." });
+  } catch (error) {
+    res.status(400).json({ message: "Error occured.", error });
+  }
+});
+
+router.get("/followers/:user_id", async (req, res) => {
+  const { user_id } = req.params;
+
+  const allFollowers = [];
+
+  try {
+    const followers = await db
+      .select("*")
+      .from("users")
+      .leftOuterJoin(
+        "follow_request",
+        "follow_request.follow_user_id",
+        "users.user_id"
+      )
+      .leftOuterJoin(
+        "profile_image_files",
+        "profile_image_files.user_id",
+        "follow_request.follow_user_id"
+      )
+      .where(function () {
+        this.where("follow_request.user_id", "=", user_id).andWhere(
+          "follow_request.approve_request",
+          "=",
+          true
+        );
+      });
+
+    followers.forEach((singleFollower) => {
+      const follower = {
+        user_id: singleFollower.user_id,
+        firstname: singleFollower.firstname,
+        lastname: singleFollower.lastname,
+      };
+      if (singleFollower.profile_image_id)
+        follower.profile_imageUri =
+          process.env.ASSETS_BASE_URL + singleFollower.profile_filename;
+
+      allFollowers.push(follower);
+    });
+
+    res.status(200).json({ message: "Followers", allFollowers });
+  } catch (error) {
+    res.status(400).json({ message: "Error occured.", error });
+  }
+});
+
+router.get("/following/:user_id", async (req, res) => {
+  const { user_id } = req.params;
+
+  const allFollowings = [];
+
+  try {
+    const followings = await db
+      .select("*")
+      .from("users")
+      .leftOuterJoin(
+        "follow_request",
+        "follow_request.user_id",
+        "users.user_id"
+      )
+      .leftOuterJoin(
+        "profile_image_files",
+        "profile_image_files.user_id",
+        "follow_request.user_id"
+      )
+      .where(function () {
+        this.where("follow_request.follow_user_id", "=", user_id).andWhere(
+          "follow_request.approve_request",
+          "=",
+          true
+        );
+      });
+
+    followings.forEach((singleFollowing) => {
+      const following = {
+        user_id: singleFollowing.user_id,
+        firstname: singleFollowing.firstname,
+        lastname: singleFollowing.lastname,
+      };
+      if (singleFollowing.profile_image_id)
+        following.profile_imageUri =
+          process.env.ASSETS_BASE_URL + singleFollowing.profile_filename;
+
+      allFollowings.push(following);
+    });
+
+    res.status(200).json({ message: "Followings.", allFollowings });
   } catch (error) {
     res.status(400).json({ message: "Error occured.", error });
   }
