8861b6b8695632ff25f67cea5ce00fc11ba636d7
ZaidAli3642 <zaidali36422@gmail.com>
2022-06-03 23:47:34 +0500

    Adding database config

--- routes/database.js
+++ routes/database.js
@@ -2,12 +2,16 @@ const knex = require("knex");
 
 const db = knex({
   client: "pg",
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   connection: {
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    password: "test",
-    database: "devarena",
+    host: process.env.DATABASE_HOST || "localhost",
+    port: process.env.DATABASE_PORT || 5432,
+    user: process.env.DATABASE_USER || "postgres",
+    password: process.env.DATABASE_PASSWORD || "test",
+    database: process.env.DATABASE_NAME || "devarena",
   },
 });
 
