3e64c577ffce85af44e60f727ea84ae0bda3cef7
ZaidAli3642 <zaidali36422@gmail.com>
2022-06-03 22:21:05 +0500

    Adding port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const config = require("config");
 const express = require("express");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 // app.use(expressfileupload());
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 
 app.use("/api", registerUserRoute);
 app.use("/api", loginUserRoute);
